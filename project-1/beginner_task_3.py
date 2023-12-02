cba4962fbb15dae0e298c584ff8c40461e7a611d
FavourAdeboye <149948883+FavourAdeboye@users.noreply.github.com>
2023-12-02 14:18:39 +0100

    New changes to code

--- project-1/beginner_task_3.py
+++ project-1/beginner_task_3.py
@@ -1,4 +1,4 @@
-#Display a Welcome message
+# Display a Welcome message
 print ('Hello World!')
 
 # Prompt the user to enter their name and store it in the 'name' variable
@@ -11,7 +11,7 @@ age = input ('Enter your age: ')
 is_student = input ('Are you a student? (yes/no): ') 
 
 # Convert the 'is_student' response to lowercase and check if it is 'yes', storing the result in 'is_student_bool'
-is_student_bool = is_student.lower() == 'yes'
+is_student_bool = is_student.lower() == "yes"
 
 # Display a message including the entered name, age, and whether the person is a student or not
 print (name, 'is', age, 'years old. Is', name, 'a student?:', is_student_bool)
